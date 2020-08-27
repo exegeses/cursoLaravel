99f0b06830367748126d4ce968d4309bd445d4e2
marcos pinardi <exegeses@gmail.com>
2020-08-27 19:51:40 -0300

    parar dato a una pericion

--- proyecto1/resources/views/adminRegiones.blade.php
+++ proyecto1/resources/views/adminRegiones.blade.php
@@ -21,7 +21,7 @@
                 <td>{{ $region->regID }}</td>
                 <td>{{ $region->regNombre }}</td>
                 <td>
-                    <a href="" class="btn btn-outline-secondary">
+                    <a href="/modificarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
                         Modificar
                     </a>
                 </td>
